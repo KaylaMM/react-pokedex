7fdff10d7d8e40cf057fee84dfa0eea676c4bacd
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-09-30 23:41:07 -0400

    fixed node modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Pokedex from './components/Pokedex';
+import React, { Component } from "react";
+import Pokecard from "./components/Pokecard";
 
-function App() {
-  return (
-    <div className="App">
-      <Pokedex />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokecard id={4} name="Charmander" type="fire" exp={62} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-
-const Pokecard = ({name, image, type}) => {
-
-  return (
-    <div className="pokecard">
-    <img src='' alt=''/>
-      <p>{name}</p>
-      <p>{type}</p>
-    </div>
-  );
-};
+class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${this.props.id}`;
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+        <img alt="poke" src={imgSrc} />
+      </div>
+    );
+  }
+}
 
 export default Pokecard;
